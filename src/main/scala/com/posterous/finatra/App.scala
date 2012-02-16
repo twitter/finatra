cea7b27972d4f477d13c167293a72a00f1438282
Julio Capote <jcapote@gmail.com>
2012-02-16 15:55:40 -0800

    init

---
+++ src/main/scala/com/posterous/finatra/App.scala
@@ -0,0 +1,15 @@
+package com.posterous.finatra
+
+/**
+ * @author ${user.name}
+ */
+object App {
+  
+  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  
+  def main(args : Array[String]) {
+    println( "Hello World!" )
+    println("concat arguments = " + foo(args))
+  }
+
+}
