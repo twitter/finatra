48f6c4b7492d862b8b8679772363407e3b5a091e
Julio Capote <jcapote@gmail.com>
2012-02-16 16:19:34 -0800

    hello world

--- src/main/scala/com/posterous/finatra/App.scala
+++ src/main/scala/com/posterous/finatra/App.scala
@@ -1,15 +1,41 @@
 package com.posterous.finatra
 
+import com.twitter.finagle.{Service, SimpleFilter}
+import org.jboss.netty.handler.codec.http._
+import org.jboss.netty.handler.codec.http.HttpResponseStatus._
+import org.jboss.netty.handler.codec.http.HttpVersion.HTTP_1_1
+import org.jboss.netty.buffer.ChannelBuffers.copiedBuffer
+import org.jboss.netty.util.CharsetUtil.UTF_8
+import com.twitter.util.Future
+import java.net.InetSocketAddress
+import com.twitter.finagle.builder.{Server, ServerBuilder}
+import com.twitter.finagle.http.Http
 /**
  * @author ${user.name}
  */
 object App {
-  
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
+ 
+
+  class HelloWorld extends Service[HttpRequest, HttpResponse] {
+    def apply(request: HttpRequest) = {
+      val response = new DefaultHttpResponse(HTTP_1_1, OK)
+      response.setContent(copiedBuffer("hello world", UTF_8))
+      Future.value(response)
+    }
+  }
+
+
   def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
+    val helloworld = new HelloWorld
+
+    val server: Server = ServerBuilder()
+      .codec(Http())
+      .bindTo(new InetSocketAddress(7070))
+      .name("helloworld")
+      .build(helloworld)
+
+    println("started on 7070")
   }
 
 }
+
