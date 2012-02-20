04a10c71e68331c9e1a507a9735c099b531b1b4f
Julio Capote <jcapote@gmail.com>
2012-02-19 16:53:52 -0800

    the rest of the prefixes

--- src/main/scala/com/posterous/finatra/FinatraApp.scala
+++ src/main/scala/com/posterous/finatra/FinatraApp.scala
@@ -3,13 +3,13 @@ import com.codahale.logula.Logging
 
 
 class FinatraApp(var prefix: String = "") extends Logging { 
-    
+     
     def get(path: String)(callback: => Any)    { Router.addRoute("GET", prefix + path)(callback) } 
-    def delete(path: String)(callback: => Any) { Router.addRoute("DELETE", path)(callback) } 
-    def post(path: String)(callback: => Any)   { Router.addRoute("POST", path)(callback) } 
-    def put(path: String)(callback: => Any)    { Router.addRoute("PUT", path)(callback) } 
-    def head(path: String)(callback: => Any)   { Router.addRoute("HEAD", path)(callback) } 
-    def patch(path: String)(callback: => Any)  { Router.addRoute("PATCH", path)(callback) } 
+    def delete(path: String)(callback: => Any) { Router.addRoute("DELETE", prefix + path)(callback) } 
+    def post(path: String)(callback: => Any)   { Router.addRoute("POST", prefix + path)(callback) } 
+    def put(path: String)(callback: => Any)    { Router.addRoute("PUT", prefix + path)(callback) } 
+    def head(path: String)(callback: => Any)   { Router.addRoute("HEAD", prefix + path)(callback) } 
+    def patch(path: String)(callback: => Any)  { Router.addRoute("PATCH", prefix + path)(callback) } 
 
     def params(x:String) = {
       Router.params(x)
