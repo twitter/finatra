df597ee3b39b13448f8462b524dbe2e5f537880d
Julio Capote <julio@twitter.com>
2012-11-04 09:05:56 -0800

    let multipart exceptions bubble up

--- src/main/scala/com/twitter/finatra/MultipartParsing.scala
+++ src/main/scala/com/twitter/finatra/MultipartParsing.scala
@@ -32,7 +32,6 @@ object MultipartParsing {
       val boundaryIndex = ctype.indexOf("boundary=")
       val boundary = ctype.substring(boundaryIndex + 9).getBytes
       val input = request.getInputStream()
-      try {
         val multistream = new MultipartStream(input, boundary)
         var nextPart = multistream.skipPreamble
         while(nextPart){
@@ -45,10 +44,6 @@ object MultipartParsing {
           multiParams = multiParams + Tuple2(name, fileobj)
           nextPart = multistream.readBoundary
         }
-      } catch {
-          case e: MultipartStream.MalformedStreamException => println("wrong")
-          case e: IOException => println("error")
-      }
     }
     multiParams
   }
