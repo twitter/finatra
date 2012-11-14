90838c4a0dd4089b8d51feb9ab88dcd317099713
Mairbek Khadikov <mkhadikov@gmail.com>
2012-11-15 00:38:04 +0200

    Routing based on filters

---
+++ src/main/scala/com/twitter/finatra/Routes.scala
@@ -0,0 +1,43 @@
+package com.twitter.finatra
+
+import com.twitter.util.Future
+import com.twitter.finagle.{Service, SimpleFilter}
+
+// TODO maybe delete this trait?
+trait Router {
+
+  def matches: Request => Option[Request]
+
+  def process: Request => Future[Response]
+
+}
+
+object Router {
+  def apply(matcher: Request => Option[Request], server: Request => Future[Response]) = new Router {
+    def process = server
+
+    def matches = matcher
+  }
+}
+
+class RouterFilter(router: Router) extends SimpleFilter[Request, Response] {
+  def apply(originalRequest: Request, service: Service[Request, Response]) =
+    router.matches(originalRequest) match {
+      case Some(request) => router.process(request)
+      case None => service(originalRequest)
+    }
+}
+
+object RouterFilter {
+  def apply(router: Router) = new RouterFilter(router)
+}
+
+class ErrorHandlerFilter(handler: PartialFunction[Throwable, Future[Response]]) extends SimpleFilter[Request, Response] {
+  def apply(originalRequest: Request, service: Service[Request, Response]) = try {
+    service(originalRequest)
+  } catch handler
+}
+
+object ErrorHandlerFilter {
+  def apply(handler: PartialFunction[Throwable, Future[Response]]) = new ErrorHandlerFilter(handler)
+}
---
+++ src/test/scala/com/twitter/finatra/FilterExampleSpec.scala
@@ -0,0 +1,131 @@
+package com.twitter.finatra
+
+import test.{FilterSpecHelper, SpecHelper}
+
+/* This test is used as the base for generating the
+ README.markdown, all new generated apps, and the finatra_example repo
+ */
+
+class FilterExampleSpec extends FilterSpecHelper {
+
+  /* ###BEGIN_APP### */
+
+  class ExampleApp extends FilteredController {
+
+    /**
+     * Basic Example
+     *
+     * curl http://localhost:7070/hello => "hello world"
+     */
+    get("/") { request =>
+      render.plain("hello world").toFuture
+    }
+
+    /**
+     * Route parameters
+     *
+     * curl http://localhost:7070/user/dave => "hello dave"
+     */
+    get("/user/:username") { request =>
+      val username = request.routeParams.getOrElse("username", "default_user")
+      render.plain("hello " + username).toFuture
+    }
+
+    /**
+     * Setting Headers
+     *
+     * curl -I http://localhost:7070/headers => "Foo:Bar"
+     */
+    get("/headers") { request =>
+      render.plain("look at headers").header("Foo", "Bar").toFuture
+    }
+
+    /**
+     * Rendering json
+     *
+     * curl -I http://localhost:7070/headers => "Foo:Bar"
+     */
+    get("/data.json") { request =>
+      render.json(Map("foo" -> "bar")).toFuture
+    }
+
+    /**
+     * Query params
+     *
+     * curl http://localhost:7070/search?q=foo => "no results for foo"
+     */
+    get("/search") { request =>
+      request.params.get("q") match {
+        case Some(q) => render.plain("no results for "+ q).toFuture
+        case None    => render.plain("query param q needed").status(500).toFuture
+      }
+    }
+
+    /**
+     * Uploading files
+     *
+     * curl -F avatar=@/path/to/img http://localhost:7070/profile
+     */
+    post("/profile") { request =>
+      request.multiParams.get("avatar").map { avatar =>
+        println("content type is " + avatar.contentType)
+        avatar.writeToFile("/tmp/avatar") //writes uploaded avatar to /tmp/avatar
+      }
+      render.plain("ok").toFuture
+    }
+
+    /**
+     * Rendering views
+     *
+     * curl http://localhost:7070/posts
+     */
+    class AnView extends View {
+      val template = "an_view.mustache"
+      val some_val = "random value here"
+    }
+
+    get("/template") { request =>
+      val anView = new AnView
+      render.view(anView).toFuture
+    }
+  }
+
+  val app = new ExampleApp
+
+  /* ###END_APP### */
+
+
+  /* ###BEGIN_SPEC### */
+
+  "GET /hello" should "respond with hello world" in {
+    get("/")
+    response.body should equal ("hello world")
+  }
+
+  "GET /user/foo" should "responsd with hello foo" in {
+    get("/user/foo")
+    response.body should equal ("hello foo")
+  }
+
+  "GET /headers" should "respond with Foo:Bar" in {
+    get("/headers")
+    response.getHeader("Foo") should equal("Bar")
+  }
+
+  "GET /data.json" should """respond with {"foo":"bar"}""" in {
+    get("/data.json")
+    response.body should equal("""{"foo":"bar"}""")
+  }
+
+  "GET /search?q=foo" should "respond with no results for foo" in {
+    get("/search?q=foo")
+    response.body should equal("no results for foo")
+  }
+
+  "GET /template" should "respond with a rendered template" in {
+    get("/template")
+    response.body should equal("Your value is random value here")
+  }
+
+  /* ###END_SPEC### */
+}
