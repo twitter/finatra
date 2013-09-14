bc20bd5720160594c48d23512350d3cc7e62747f
Phil Calçado <pcalcado@gmail.com>
2013-09-13 20:45:05 -0400

    Removed magical strings from configuration lookup
    Signed-off-by: Julio Capote <julio@twitter.com>
    
    Conflicts:
    	src/main/scala/com/twitter/finatra/FinatraServer.scala

--- src/main/scala/com/twitter/finatra/Logging.scala
+++ src/main/scala/com/twitter/finatra/Logging.scala
@@ -18,7 +18,7 @@ package com.twitter.finatra
 import com.twitter.logging.Logger
 
 trait Logging {
-  val logger = Logger.get(Config.get("log_node"))
+  val logger = Logger.get(Config.get(FinatraParams.logNode))
 
   def appendCollection[A,B](buf: StringBuilder, x: Map[A,B]) {
     x foreach { xs =>
