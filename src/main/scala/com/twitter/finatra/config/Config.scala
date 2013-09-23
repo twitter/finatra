c5ad557872f547a46d5d6837a16c5905d0daed45
Julio Capote <julio@twitter.com>
2013-09-22 22:43:05 -0700

    default to off for pid file

--- src/main/scala/com/twitter/finatra/config/Config.scala
+++ src/main/scala/com/twitter/finatra/config/Config.scala
@@ -4,11 +4,10 @@ import com.twitter.app.GlobalFlag
 
 object port            extends GlobalFlag[String](":7070", "Http Port")
 object adminPort       extends GlobalFlag[String](":9990", "Admin/Stats Port")
-object sslPort         extends GlobalFlag[String](":7443", "Port for SSL")
+object sslPort         extends GlobalFlag[String](":7443", "Https Port")
 object env             extends GlobalFlag[String]("development", "Environment")
-object appName         extends GlobalFlag[String]("finatra", "Name of server")
 object pidEnabled      extends GlobalFlag[Boolean](false, "whether to write pid file")
-object pidPath         extends GlobalFlag[String]("finatra.pid", "path to pid file")
+object pidPath         extends GlobalFlag[String]("", "path to pid file")
 object logPath         extends GlobalFlag[String]("logs/finatra.log", "path to log")
 object logLevel        extends GlobalFlag[String]("INFO", "log level")
 object logNode         extends GlobalFlag[String]("finatra", "Logging node")
