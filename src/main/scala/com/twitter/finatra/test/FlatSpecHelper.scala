8fdc1835c49da086d1825c6b8d8427aab849c3e6
Julio Capote <julio@twitter.com>
2013-09-22 22:33:16 -0700

    fix package for FlatSpecHelper

--- src/main/scala/com/twitter/finatra/test/FlatSpecHelper.scala
+++ src/main/scala/com/twitter/finatra/test/FlatSpecHelper.scala
@@ -3,4 +3,4 @@ package com.twitter.finatra.test
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
 
-abstract class FlatSpecHelper extends FlatSpec with ShouldMatchers with SpecHelper
+trait FlatSpecHelper extends FlatSpec with ShouldMatchers with SpecHelper
