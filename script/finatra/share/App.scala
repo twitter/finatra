c3bd3bc930dc7380ce9ee129c9c29ca00d38ec2f
Julio Capote <julio@twitter.com>
2013-10-13 19:47:27 -0700

    fix app generator

--- script/finatra/share/App.scala
+++ script/finatra/share/App.scala
@@ -2,14 +2,10 @@ package ###PACKAGE_NAME###
 
 import com.twitter.finatra._
 import com.twitter.finatra.ContentType._
-import com.twitter.ostrich.stats.Stats
 
-object App {
+object App extends FinatraServer {
 
   __EXAMPLEAPP__
 
-  def main(args: Array[String]) = {
-    FinatraServer.register(app)
-    FinatraServer.start()
-  }
+  register(app)
 }
