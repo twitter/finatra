fb884d885c8e17e590b0eb561e1ebdd0890827bb
Matt <matt.ho@gmail.com>
2013-11-05 09:15:34 -0800

    - added Twitter license at the top

--- src/main/scala/com/twitter/finatra/test/MockApp.scala
+++ src/main/scala/com/twitter/finatra/test/MockApp.scala
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2012 Twitter Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.twitter.finatra.test
 
 import com.twitter.finatra._
@@ -12,9 +27,6 @@ import java.util
 import org.jboss.netty.buffer.ChannelBuffers
 import java.net.URLEncoder
 
-/**
- * @author matt.ho@gmail.com
- */
 case class MockApp(controller: Controller) {
   def buildRequest(method: HttpMethod, path: String, params: Map[String, String] = Map(), headers: Map[String, String] = Map(), body: AnyRef = null): FinagleRequest = {
 
