0b3c7e0a03a0e26e78dfbe80cb365c18cf52d127
Tom Czarniecki <github@tomczarniecki.com>
2014-05-14 03:31:27 -0400

    Fix for mustache templates that don't load in production environments. Nested templates will NOT have the '.mustache' file extension which means they will not load. In addition, FileResolver already handles development/production environment loading strategies properly and is already used by the View class, so we should use it in the FinatraMustacheFactory as well.

--- src/test/scala/com/twitter/finatra/ViewSpec.scala
+++ src/test/scala/com/twitter/finatra/ViewSpec.scala
@@ -19,4 +19,17 @@ class ViewSpec extends ShouldSpec {
     view.render should include ("please &lt;escape&gt; me")
     view.render should include ("<div>") // prove we don't escape the partial's content
   }
+
+  "A layout" should "render in production mode" in {
+    System.setProperty("com.twitter.finatra.config.env", "production")
+    try {
+      val posts = List(new Post("One"), new Post("Two"))
+      val view  = new PostsListView(posts)
+
+      view.render should include ("Hello World")
+      view.render should include ("This is Base.")
+    } finally {
+      System.setProperty("com.twitter.finatra.config.env", "development")
+    }
+  }
 }
