f776b80a06c39310ad08bf2eb4fdec2fc9a8e804
dvarelap <dvarelap@gmail.com>
2014-06-06 11:55:52 -0700

    Enables session support for Finatra in a way extends Controller with SessionEnabled

---
+++ src/main/scala/com/twitter/finatra/Session.scala
@@ -0,0 +1,55 @@
+package com.twitter.finatra
+
+import java.util.UUID
+import com.twitter.finagle.http.Cookie
+import scala.collection.mutable
+
+trait SessionEnabled {
+  def session(implicit req: Request): Session = {
+    val sessionCookie: SessionCookie = req.cookies.get("_session_id") match {
+      case None    => SessionCookie()
+      case Some(c) => SessionCookie(c.value)
+    }
+
+    req.response.addCookie(sessionCookie)
+    SessionHolder.getOrCreateSession(sessionCookie)
+  }
+}
+
+object SessionCookie {
+  def apply() = new SessionCookie(newSessionId)
+
+  def apply(value: String) = new SessionCookie(value)
+
+  private def newSessionId = UUID.randomUUID.toString
+}
+
+case class SessionCookie(cookieValue: String, isHttpOnly: Boolean = true, secure: Boolean = false)
+  extends Cookie("_session_id", cookieValue) {
+
+  override def httpOnly = isHttpOnly
+
+  override def isSecure = secure
+}
+
+protected class Session(val sessionId: String) {
+  private val values = mutable.Map[String, String]()
+
+  def get(key: String): Option[String] = values.get(key)
+
+  def put(key: String, value: String) {
+    values(key) = value
+  }
+}
+
+object SessionHolder {
+  private val sessions = mutable.Map[SessionCookie, Session]()
+
+  private def createAndAddSession(c: SessionCookie) = {
+    val context = new Session(c.value)
+    sessions(c) = context
+    context
+  }
+
+  def getOrCreateSession(c: SessionCookie) = sessions.getOrElse(c, createAndAddSession(c))
+}
---
+++ src/test/scala/com/twitter/finatra/SessionSpec.scala
@@ -0,0 +1,48 @@
+package com.twitter.finatra
+
+import com.twitter.finatra.test.FlatSpecHelper
+
+class SessionEnabledApp extends Controller with SessionEnabled {
+
+  get("/new_session") { implicit request =>
+    session.put("foo", "bar")
+    render.plain("wooo").toFuture
+  }
+
+  get("/existing_session") { implicit request =>
+    render.plain(session.get("foo").getOrElse("<empty>")).toFuture
+  }
+}
+
+class SessionSpec extends FlatSpecHelper {
+
+  val server = new FinatraServer
+  server.register(new SessionEnabledApp)
+
+  "basic session creation" should "have _session_id set" in {
+    get("/new_session")
+    response.getHeader("Set-Cookie") should startWith("_session_id=")
+  }
+
+  "session retrieve" should "retrieve an existing session for a given _session_id" in {
+    val session = SessionHolder.getOrCreateSession(SessionCookie("TEST_SESSION_ID"))
+    session.put("foo", "bar")
+    get("/existing_session", headers = Map("Cookie" -> "_session_id=TEST_SESSION_ID"))
+    response.body should be("bar")
+  }
+
+  "#get" should "retrieve Some(value) or None" in {
+    val session = new Session("TEST_SESSION_ID")
+    session.get("nothing") should be(None)
+    session.put("foo", "bar")
+    session.get("foo") should be(Some("bar"))
+  }
+
+  "#put" should "set or update values in session" in {
+    val session = new Session("TEST_SESSION_ID")
+    session.put("foo", "bar")
+    session.get("foo") should be(Some("bar"))
+    session.put("foo", "xyz")
+    session.get("foo") should be(Some("xyz"))
+  }
+}
