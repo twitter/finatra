9db122401e919f1ac33b36de35f304cce4f5e21f
Manju Rajashekhar <manj@cs.stanford.edu>
2014-10-14 16:21:56 -0700

    MultipartParsing should only be called for POST requests that are multipart

--- src/main/scala/com/twitter/finatra/RequestAdapter.scala
+++ src/main/scala/com/twitter/finatra/RequestAdapter.scala
@@ -17,6 +17,7 @@ package com.twitter.finatra
 
 import com.twitter.finagle.http.{Request => FinagleRequest}
 import org.jboss.netty.handler.codec.http.HttpMethod
+import org.jboss.netty.handler.codec.http.multipart.HttpPostRequestDecoder
 
 /**
 * Adapts a FinagleRquest to a FinatraRequest
@@ -28,7 +29,7 @@ object RequestAdapter {
 
     request.getContent.markReaderIndex
 
-    if (request.method == HttpMethod.POST) {
+    if (request.method == HttpMethod.POST && HttpPostRequestDecoder.isMultipart(request)) {
       request.multiParams = MultipartParsing(request)
     }
 
