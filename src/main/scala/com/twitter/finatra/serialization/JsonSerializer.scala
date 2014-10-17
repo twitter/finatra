7d79ef57e471bbb2062e63210cedee9efc50e62a
Michael Rose <elementation@gmail.com>
2014-10-17 01:23:21 -0600

    JsonSerializer: Removing author tags

--- src/main/scala/com/twitter/finatra/serialization/JsonSerializer.scala
+++ src/main/scala/com/twitter/finatra/serialization/JsonSerializer.scala
@@ -1,11 +1,6 @@
 package com.twitter.finatra.serialization
 
 
-/**
- * 2014-03-20
- * @author Michael Rose <michael@fullcontact.com>
- */
-
 trait JsonSerializer {
   def serialize[T](item: T): Array[Byte]
 }
