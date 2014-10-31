157da6a6f43e07ef3399defa404d86b4c5d73e16
Eugene Ma <ema@twitter.com>
2014-10-31 23:57:41 +0000

    Port Filter to Finatra
    Provides GuestId and DeviceId request scoped containers. Not part
    of standard Monorail chain or module because it's not installed in
    every endpoint. Later we should use @AssistedInjection or something
    similar to customize filter chains per controller/method.
    
    RB_ID=488907
    TBR=true

--- finatra/finatra-scopes/src/test/scala/com/twitter/finatra/requestscope/FinagleRequestScopeFilterTest.scala
+++ finatra/finatra-scopes/src/test/scala/com/twitter/finatra/requestscope/FinagleRequestScopeFilterTest.scala
@@ -3,9 +3,15 @@ package com.twitter.finatra.requestscope
 import com.twitter.finagle.Service
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.util.{Await, Future}
+import org.junit.runner.RunWith
 import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
 import org.specs2.mock.Mockito
 
+// TODO (DATAAPI-888): Something is wrong with this test. I can muck
+// around with the assertions and it will still pass! See inline
+// comments.
+@RunWith(classOf[JUnitRunner])
 class FinagleRequestScopeFilterTest
   extends FunSuite
   with Mockito {
@@ -17,7 +23,9 @@ class FinagleRequestScopeFilterTest
   test("verify enter and exit are called") {
     val service = Service.mk[Request, Response] { request: Request =>
       there was one(finagleRequestScope).enter()
+      // this will pass: there was no(finagleRequestScope).enter() 
       there was no(finagleRequestScope).exit()
+      // this will pass: there was one(finagleRequestScope).exit()
       Future(response)
     }
 
