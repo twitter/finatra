af309c4eb38722edd31e2e56f8f7a51679d5cd43
Antoine Tollenaere <atollenaere@twitter.com>
2014-12-29 21:33:59 +0000

    Remove references to c.t.f.builder.Server.localAddress
    This field is deprecated by https://reviewboard.twitter.biz/r/547434/.
    
    Use a c.t.finagle.ListeningServer rather than c.t.finagle.builder.Server where applicable.
    
    RB_ID=548652
    TBR=true

--- finatra/finatra-utils/src/main/scala/com/twitter/finatra/utils/PortUtils.scala
+++ finatra/finatra-utils/src/main/scala/com/twitter/finatra/utils/PortUtils.scala
@@ -2,23 +2,18 @@ package com.twitter.finatra.utils
 
 import com.twitter.app.Flaggable
 import com.twitter.finagle.ListeningServer
-import com.twitter.finagle.builder.{Server => BuilderServer}
 import java.net.{InetAddress, InetSocketAddress, SocketAddress}
 
 object PortUtils {
   def getLoopbackHostAddress: String =
     InetAddress.getLoopbackAddress.getHostAddress
-  
+
   def getPort(server: ListeningServer): Int = {
     server.boundAddress.asInstanceOf[InetSocketAddress].getPort
   }
 
-  def getPort(server: BuilderServer): Int = {
-    getSocketAddress(server).asInstanceOf[InetSocketAddress].getPort
-  }
-
-  def getSocketAddress(server: BuilderServer): SocketAddress = {
-    server.localAddress
+  def getSocketAddress(server: ListeningServer): SocketAddress = {
+    server.boundAddress
   }
 
   def getPort(socketAddress: SocketAddress): Int = {
