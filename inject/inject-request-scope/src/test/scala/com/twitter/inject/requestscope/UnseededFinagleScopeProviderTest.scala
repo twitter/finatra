7470634ae3cb983b8035a98f07ce864aa3aeb0ed
Alex Leong <aleong@twitter.com>
2015-03-06 00:41:23 +0000

    Add Experimental Features
    RB_ID=600134

--- inject/inject-request-scope/src/test/scala/com/twitter/inject/requestscope/UnseededFinagleScopeProviderTest.scala
+++ inject/inject-request-scope/src/test/scala/com/twitter/inject/requestscope/UnseededFinagleScopeProviderTest.scala
@@ -1,7 +1,10 @@
 package com.twitter.inject.requestscope
 
+import org.junit.runner.RunWith
 import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
 
+@RunWith(classOf[JUnitRunner])
 class UnseededFinagleScopeProviderTest extends FunSuite {
   test("expect exception") {
     intercept[IllegalStateException] {
