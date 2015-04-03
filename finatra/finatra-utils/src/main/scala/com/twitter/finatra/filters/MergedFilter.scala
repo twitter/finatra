8d1eace0d81ec6cc5c65e33a9efcfd0db1f700bb
Alex Leong <aleong@twitter.com>
2015-04-03 18:36:14 +0000

    MergedFilter changes and updates
    RB_ID=625288
    TBR=true

--- finatra/finatra-utils/src/main/scala/com/twitter/finatra/filters/MergedFilter.scala
+++ finatra/finatra-utils/src/main/scala/com/twitter/finatra/filters/MergedFilter.scala
@@ -4,13 +4,13 @@ import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finagle.{Filter, Service}
 import com.twitter.util.Future
 
-class MergedFilter(
-  filters: Filter[Request, Response, Request, Response]*)
-  extends Filter[Request, Response, Request, Response] {
+class MergedFilter[Req <: Request](
+  filters: Filter[Req, Response, Req, Response]*)
+  extends Filter[Req, Response, Req, Response] {
 
   private val CombinedFilter = filters reduceLeft {_ andThen _}
 
-  def apply(request: Request, service: Service[Request, Response]): Future[Response] = {
+  def apply(request: Req, service: Service[Req, Response]): Future[Response] = {
     CombinedFilter(request, service)
   }
 }
