a9735c2d38b4474d20195a0467870850844d0899
Eugene Ma <ema@twitter.com>
2015-07-28 00:04:35 +0000

    http: remove ExceptionMapper.replace
    Problem:
    
    ExceptionManager.replace is too fragile because it relies on module
    initialization order.
    
    Solution:
    
    Remove ExceptionManager.replace. Fold HttpException, HttpResponseException,
    CancelledRequestException into FinatraDefaultExceptionMapper to
    enforce built-in exception handling that shouldn't be configurable.
    
    Result:
    
    ExceptionManager.replace is gone. Users can write their own modules
    if the default module isn't suitable.
    
    RB_ID=720818

--- http/src/main/scala/com/twitter/http/internal/exceptions/FinatraDefaultExceptionMapper.scala
+++ http/src/main/scala/com/twitter/http/internal/exceptions/FinatraDefaultExceptionMapper.scala
@@ -3,7 +3,7 @@ package com.twitter.finatra.http.internal.exceptions
 import com.twitter.finagle.CancelledRequestException
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finatra.exceptions.ExternalServiceExceptionMatcher
-import com.twitter.finatra.http.exceptions.DefaultExceptionMapper
+import com.twitter.finatra.http.exceptions.{DefaultExceptionMapper, HttpException, HttpResponseException}
 import com.twitter.finatra.http.response.{ResponseBuilder, ErrorsResponse}
 import com.twitter.inject.Logging
 import javax.inject.{Inject, Singleton}
@@ -16,6 +16,12 @@ class FinatraDefaultExceptionMapper @Inject()(
 
   override def toResponse(request: Request, throwable: Throwable): Response = {
     throwable match {
+      case e: HttpException =>
+        e.createResponse(response)
+      case e: HttpResponseException =>
+        e.response
+      case e: CancelledRequestException =>
+        response.clientClosed
       case ExternalServiceExceptionMatcher(e) =>
         warn("service unavailable", e)
         response.serviceUnavailable.json(
