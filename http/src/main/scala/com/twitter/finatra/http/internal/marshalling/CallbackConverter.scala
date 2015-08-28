72e56db26c1035d3c1879d9f0d20d89ebe7678e0
Steve Cosenza <scosenza@twitter.com>
2015-08-28 21:59:13 +0000

    finatra-http: Remove deprecated methods in CallbackConverter
    Problem
    CallbackConverter uses the deprecated "classManifest" method
    
    Solution
    Remove usages of "classManifest" method
    
    RB_ID=735843

--- http/src/main/scala/com/twitter/finatra/http/internal/marshalling/CallbackConverter.scala
+++ http/src/main/scala/com/twitter/finatra/http/internal/marshalling/CallbackConverter.scala
@@ -5,13 +5,9 @@ import com.twitter.finagle.httpx.{Request, Response}
 import com.twitter.finatra.http.response.{ResponseBuilder, StreamingResponse}
 import com.twitter.finatra.json.FinatraObjectMapper
 import com.twitter.finatra.json.internal.streaming.JsonStreamParser
-import com.twitter.util.{Future, FuturePool}
+import com.twitter.util.Future
 import javax.inject.Inject
 
-/*
- * Note: We are using deprecated classManifest and <:< to avoid potential threading issues
- * with 2.10 reflection methods. See http://docs.scala-lang.org/overviews/reflection/thread-safety.html
- */
 class CallbackConverter @Inject()(
   messageBodyManager: MessageBodyManager,
   responseBuilder: ResponseBuilder,
@@ -28,36 +24,30 @@ class CallbackConverter @Inject()(
   /* Private */
 
   private def createRequestCallback[RequestType: Manifest, ResponseType: Manifest](callback: (RequestType) => ResponseType): (Request) => ResponseType = {
-    val requestManifest = manifest[RequestType]
-    requestManifest match {
-
-      // The callback takes a finagle Request
-      case finagleRequest if finagleRequest <:< classManifest[Request] =>
-        callback.asInstanceOf[(Request => ResponseType)]
-
-      // AsyncStream[T] //TODO Also support AsyncStream[Try[T]]
-      case stream if isAsyncStream[RequestType] =>
-        request: Request =>
-          val parsedStream = jsonStreamParser.parseArray(request.reader)(requestManifest.typeArguments.head).asInstanceOf[RequestType]
-          callback(parsedStream)
-
-      // Otherwise convert Request into the callback's input type
-      case _ =>
-        request: Request =>
-          val callbackInput = messageBodyManager.read[RequestType](request)
-          callback(callbackInput)
+    if (manifest[RequestType] == manifest[Request]) {
+      callback.asInstanceOf[(Request => ResponseType)]
+    }
+    else if (runtimeClassEq[RequestType, AsyncStream[_]]) {
+      val asyncStreamTypeParam = manifest[RequestType].typeArguments.head
+      request: Request =>
+        val asyncStream = jsonStreamParser.parseArray(request.reader)(asyncStreamTypeParam)
+        callback(asyncStream.asInstanceOf[RequestType])
+    } else {
+      request: Request =>
+        val callbackInput = messageBodyManager.read[RequestType](request)
+        callback(callbackInput)
     }
   }
 
   private def createResponseCallback[ResponseType: Manifest](requestCallback: (Request) => ResponseType): (Request) => Future[Response] = {
-    if (isFutureResponse[ResponseType]) {
+    if (manifest[ResponseType] == manifest[Future[Response]]) {
       requestCallback.asInstanceOf[(Request => Future[Response])]
     }
     else if (isFutureOption[ResponseType]) {
       request: Request =>
         requestCallback(request).asInstanceOf[Future[Option[_]]] map optionToHttpResponse
     }
-    else if (isAsyncStream[ResponseType]) {
+    else if (runtimeClassEq[ResponseType, AsyncStream[_]]) {
       request: Request =>
         val asyncStream = requestCallback(request).asInstanceOf[AsyncStream[_]]
 
@@ -67,20 +57,20 @@ class CallbackConverter @Inject()(
 
         streamingResponse.toFutureFinagleResponse
     }
-    else if (isFuture[ResponseType]) {
+    else if (runtimeClassEq[ResponseType, Future[_]]) {
       request: Request =>
         requestCallback(request).asInstanceOf[Future[_]] map createHttpResponse
     }
-    else if (isStreamingResponse[ResponseType]) {
+    else if (runtimeClassEq[ResponseType, StreamingResponse[_]]) {
       request: Request =>
         requestCallback(request).asInstanceOf[StreamingResponse[_]].toFutureFinagleResponse
     }
-    else if (isResponse[ResponseType]) {
+    else if (manifest[ResponseType] == manifest[Response]) {
       request: Request =>
         Future(
           requestCallback(request).asInstanceOf[Response])
     }
-    else if (isOption[ResponseType]) {
+    else if (runtimeClassEq[ResponseType, Option[_]]) {
       request: Request =>
         Future(
           optionToHttpResponse(requestCallback(request).asInstanceOf[Option[_]]))
@@ -105,37 +95,14 @@ class CallbackConverter @Inject()(
     }
   }
 
-  private def isFutureResponse[T: Manifest]: Boolean = {
-    manifest[T] <:< classManifest[Future[Response]]
-  }
-
-  private def isResponse[T: Manifest]: Boolean = {
-    manifest[T] <:< classManifest[Response]
-  }
-
-  private def isStreamingResponse[T: Manifest]: Boolean = {
-    manifest[T].runtimeClass == classOf[StreamingResponse[_]]
-  }
-
-  private def isAsyncStream[T: Manifest]: Boolean = {
-    manifest[T].runtimeClass == classOf[AsyncStream[_]]
+  private def runtimeClassEq[T: Manifest, U: Manifest]: Boolean = {
+    manifest[T].runtimeClass == manifest[U].runtimeClass
   }
 
   private def isFutureOption[T: Manifest]: Boolean = {
-    isFuture[T] && hasOptionTypeParam
-  }
-
-  private def isOption[T: Manifest] = {
-    manifest[T].runtimeClass == classOf[Option[_]]
-  }
-
-  private def hasOptionTypeParam[T: Manifest] = {
     val typeArgs = manifest[T].typeArguments
-    typeArgs.size == 1 &&
+    runtimeClassEq[T, Future[_]] &&
+      typeArgs.size == 1 &&
       typeArgs.head.runtimeClass == classOf[Option[_]]
-  }
-
-  private def isFuture[T: Manifest]: Boolean = {
-    manifest[T].runtimeClass == classOf[Future[_]]
   }
 }
