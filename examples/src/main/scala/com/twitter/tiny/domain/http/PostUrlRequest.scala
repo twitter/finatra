8d813ed5bc6b660ef9d1b2d9006ecefe5a393eff
Christopher Coco <ccoco@twitter.com>
2015-09-10 09:07:52 -0700

    finatra/examples: Updated for merge

--- examples/src/main/scala/com/twitter/tiny/domain/http/PostUrlRequest.scala
+++
@@ -1,8 +0,0 @@
-package com.twitter.tiny.domain.http
-
-import com.twitter.finagle.http.Request
-import com.twitter.finatra.request.RequestInject
-
-case class PostUrlRequest(
-  @RequestInject request: Request,
-  url: String)
