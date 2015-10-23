0ad8d8e3e2524891d19183226f0ba52d5401c341
Christopher Coco <ccoco@twitter.com>
2015-10-22 17:11:26 -0700

    Updated test

--- thrift/src/test/scala/com/twitter/finatra/thrift/tests/ThriftServerStartupFeatureTest.scala
+++ thrift/src/test/scala/com/twitter/finatra/thrift/tests/ThriftServerStartupFeatureTest.scala
@@ -18,13 +18,10 @@ class ThriftServerStartupFeatureTest extends Test {
       }
     })
 
-    try {
-      intercept[AssertionError] {
-        server.start()
-      }
-    }
-    finally {
-      server.close()
+    // don't call server.close() as the server never starts
+    // thus calling server.close will result in an NPE
+    intercept[AssertionError] {
+      server.start()
     }
   }
 
