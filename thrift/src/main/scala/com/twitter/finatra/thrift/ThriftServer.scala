d511d97e9b7a75dc4e5fb475280aee1861e92a71
Christopher Coco <ccoco@twitter.com>
2016-01-13 17:28:37 +0000

    thrift - Add flag and functionality for announcing Thrift Server
    Problem
    
    We don't currently expose the ability to announce a Thrift server
    akin to the functionality in the HttpServer (via BaseHttpServer).
    
    Resolution
    
    Add the 'thrift.announce' flag and functionality to invoke
    server.announce when the flag is set to bring functionality on
    par with Http support.
    
    Result
    
    Users will be able to optionally announce their Thrift server by
    setting the 'thrift.announce' flag appropriately.
    
    RB_ID=787543

--- thrift/src/main/scala/com/twitter/finatra/thrift/ThriftServer.scala
+++ thrift/src/main/scala/com/twitter/finatra/thrift/ThriftServer.scala
@@ -9,6 +9,7 @@ trait ThriftServer extends TwitterServer {
 
   private val thriftPortFlag = flag("thrift.port", ":9999", "External Thrift server port")
   private val thriftShutdownTimeout = flag("thrift.shutdown.time", 1.minute, "Maximum amount of time to wait for pending requests to complete on shutdown")
+  private val thriftAnnounceFlag = flag[String]("thrift.announce", "Address to announce Thrift server to")
 
   /* Private Mutable State */
   private var thriftServer: ListeningServer = _
@@ -30,6 +31,7 @@ trait ThriftServer extends TwitterServer {
       Await.result(
         close(thriftServer, thriftShutdownTimeout().fromNow))
     }
+    for (addr <- thriftAnnounceFlag.get) thriftServer.announce(addr)
     info("Thrift server started on port: " + thriftPort.get)
   }
 
