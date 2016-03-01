6e940b1b378fbda87101c211708dd6fcdee7cd64
Ryan O'Neill <ryano@twitter.com>
2016-03-01 20:11:36 +0000

    finatra/http: Refactor Template Lookup to New Class
    Problem
    
    Looking up which mustache file is associated with a particular
    class is currently an implementation detail of
    MustacheMessageBodyWriter. It would be beneficial to separate
    out the logic and be able to reuse it internally.
    
    Solution
    
    Refactor MustacheMessageBodyWriter to use a
    MustacheTemplateNameLookup instance where the lookup and caching
    logic now reside.
    
    RB_ID=805317

--- http/src/main/scala/com/twitter/finatra/http/internal/marshalling/mustache/MustacheMessageBodyWriter.scala
+++ http/src/main/scala/com/twitter/finatra/http/internal/marshalling/mustache/MustacheMessageBodyWriter.scala
@@ -3,54 +3,33 @@ package com.twitter.finatra.http.internal.marshalling.mustache
 import com.google.common.net.MediaType
 import com.twitter.finatra.http.marshalling.mustache.{MustacheBodyComponent, MustacheService}
 import com.twitter.finatra.http.marshalling.{MessageBodyWriter, WriterResponse}
-import com.twitter.finatra.response.Mustache
-import com.twitter.inject.conversions.map._
 import com.twitter.io.Buf
-import java.util.concurrent.ConcurrentHashMap
 import javax.inject.{Inject, Singleton}
-import scala.collection.JavaConverters._
 
 @Singleton
 class MustacheMessageBodyWriter @Inject()(
-  mustacheService: MustacheService)
+  mustacheService: MustacheService,
+  templateLookup: MustacheTemplateNameLookup)
   extends MessageBodyWriter[Any] {
-
-  private val classToViewNameCache = new ConcurrentHashMap[Class[_], String]().asScala
 
   /* Public */
 
   override def write(obj: Any): WriterResponse = {
     WriterResponse(
       MediaType.HTML_UTF_8,
-      obj match {
-        case c: MustacheBodyComponent => createBuffer(c)
-        case _ => createBuffer(obj)
-      }
+      mustacheService.createBuffer(
+        templateLookup.getTemplateName(obj),
+        getScope(obj))
     )
   }
 
   /* Private */
 
-  private def createBuffer(mustacheBodyComponent: MustacheBodyComponent): Buf = {
-    val template = if (mustacheBodyComponent.template.isEmpty) {
-      lookupTemplateName(mustacheBodyComponent.data)
-    } else {
-      mustacheBodyComponent.template
+  private def getScope(obj: Any): Any = {
+    obj match {
+      case c: MustacheBodyComponent => c.data
+      case _ => obj
     }
-    mustacheService.createBuffer(template, mustacheBodyComponent.data)
   }
 
-  private def createBuffer(obj: Any): Buf = {
-    mustacheService.createBuffer(
-      lookupTemplateName(obj),
-      obj
-    )
-  }
-
-  private def lookupTemplateName(viewObj: Any): String = {
-    classToViewNameCache.atomicGetOrElseUpdate(viewObj.getClass, {
-      val mustacheAnnotation = viewObj.getClass.getAnnotation(classOf[Mustache])
-      mustacheAnnotation.value + ".mustache"
-    })
-  }
 }
---
+++ http/src/main/scala/com/twitter/finatra/http/internal/marshalling/mustache/MustacheTemplateNameLookup.scala
@@ -0,0 +1,34 @@
+package com.twitter.finatra.http.internal.marshalling.mustache
+
+import com.twitter.finatra.http.marshalling.mustache.MustacheBodyComponent
+import com.twitter.finatra.response.Mustache
+import com.twitter.inject.conversions.map._
+import java.util.concurrent.ConcurrentHashMap
+import javax.inject.Singleton
+import scala.collection.JavaConverters._
+
+@Singleton
+class MustacheTemplateNameLookup {
+
+  private val classToTemplateNameCache = new ConcurrentHashMap[Class[_], String]().asScala
+
+  /* Public */
+
+  def getTemplateName(obj: Any): String = {
+    obj match {
+      case mbc: MustacheBodyComponent if !mbc.template.isEmpty => mbc.template
+      case mbc: MustacheBodyComponent => lookupViaAnnotation(mbc.data)
+      case _ => lookupViaAnnotation(obj)
+    }
+  }
+
+  /* Private */
+
+  private def lookupViaAnnotation(viewObj: Any): String = {
+    classToTemplateNameCache.atomicGetOrElseUpdate(viewObj.getClass, {
+      val mustacheAnnotation = viewObj.getClass.getAnnotation(classOf[Mustache])
+      mustacheAnnotation.value + ".mustache"
+    })
+  }
+
+}
