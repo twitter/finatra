97343495b26c3d341e5f3bb2a206940a8e31026b
Christopher Coco <ccoco@twitter.com>
2016-11-22 01:53:55 +0000

    finatra-http: Update DarkTrafficTestServerFeatureTest
    Problem/Solution
    
    The DarkTrafficTestServerFeatureTest sometimes fails as it is
    sensitive to the order of server initialization. Let's ensure
    the dark server is up and healthy before issuing the request
    to the live server.
    
    RB_ID=892392

--- http/src/test/scala/com/twitter/finatra/http/tests/integration/darktraffic/test/DarkTrafficTestServerFeatureTest.scala
+++ http/src/test/scala/com/twitter/finatra/http/tests/integration/darktraffic/test/DarkTrafficTestServerFeatureTest.scala
@@ -1,24 +1,13 @@
 package com.twitter.finatra.http.tests.integration.darktraffic.test
 
-import com.google.inject.testing.fieldbinder.Bind
-import com.twitter.finagle.Service
-import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finagle.http.Status._
-import com.twitter.finatra.annotations.DarkTrafficService
-import com.twitter.finatra.http.{EmbeddedHttpServer, HttpHeaders}
+import com.twitter.finatra.http.EmbeddedHttpServer
 import com.twitter.finatra.http.tests.integration.darktraffic.main.DarkTrafficTestServer
 import com.twitter.inject.Mockito
 import com.twitter.inject.server.{FeatureTest, PortUtils}
-import com.twitter.util.Future
-import org.mockito.ArgumentCaptor
+import org.scalatest.concurrent.Eventually._
 
 class DarkTrafficTestServerFeatureTest extends FeatureTest with Mockito {
-
-  @Bind
-  @DarkTrafficService
-  val darkTrafficService: Option[Service[Request, Response]] = Some(smartMock[Service[Request, Response]])
-
-  darkTrafficService.get.apply(any[Request]).returns(Future.value(smartMock[Response]))
 
   // receive dark traffic service
   override val server = new EmbeddedHttpServer(
@@ -30,28 +19,18 @@ class DarkTrafficTestServerFeatureTest extends FeatureTest with Mockito {
     twitterServer = new DarkTrafficTestServer {
       override val name = "live-server"
     },
-    flags = Map(
-      "http.dark.service.dest" -> s"/$$/inet/${PortUtils.loopbackAddress}/${server.httpExternalPort}"))
+    flags =
+      Map(
+      "http.dark.service.dest" ->
+        s"/$$/inet/${PortUtils.loopbackAddress}/${server.httpExternalPort}"))
 
   "DarkTrafficServer" should {
-
-    // Canonical-Resource header is used by Diffy Proxy
-    "have Canonical-Resource header correctly set" in {
-      server.httpGet(
-        "/plaintext",
-        withBody = "Hello, World!",
-        andExpect = Ok)
-
-      val captor = ArgumentCaptor.forClass(classOf[Request])
-      there was one(darkTrafficService.get).apply(captor.capture())
-      val request = captor.getValue
-      request.headerMap(HttpHeaders.CanonicalResource) should be("GET_/plaintext")
-    }
 
     // See SampleDarkTrafficFilterModule#enableSampling
     "Get method is forwarded" in {
       liveServer.httpGet(
         "/plaintext",
+        andExpect = Ok,
         withBody = "Hello, World!")
 
       // service stats
@@ -61,9 +40,11 @@ class DarkTrafficTestServerFeatureTest extends FeatureTest with Mockito {
       liveServer.assertCounter("dark_traffic_filter/forwarded", 1)
       liveServer.assertCounter("dark_traffic_filter/skipped", 0)
 
-      server.assertHealthy() // stat to be recorded on the dark service
+      server.assertHealthy()
       // "dark" service stats
-      server.assertCounter("route/plaintext/GET/status/200", 1)
+      eventually {
+        server.assertCounter("route/plaintext/GET/status/200", 1)
+      }
     }
 
     "Put method is forwarded" in {
@@ -81,9 +62,11 @@ class DarkTrafficTestServerFeatureTest extends FeatureTest with Mockito {
       liveServer.assertCounter("dark_traffic_filter/forwarded", 1)
       liveServer.assertCounter("dark_traffic_filter/skipped", 0)
 
-      server.assertHealthy() // stat to be recorded on the dark service
+      server.assertHealthy()
       // "dark" service stats
-      server.assertCounter("route/echo/PUT/status/200", 1)
+      eventually {
+        server.assertCounter("route/echo/PUT/status/200", 1)
+      }
     }
 
     "Post method not forwarded" in {
@@ -100,7 +83,7 @@ class DarkTrafficTestServerFeatureTest extends FeatureTest with Mockito {
       liveServer.assertCounter("dark_traffic_filter/forwarded", 0)
       liveServer.assertCounter("dark_traffic_filter/skipped", 1)
 
-      server.assertHealthy() // stat to be recorded on the dark service
+      server.assertHealthy()
       // "dark" service stats
       server.assertCounter("route/foo/POST/status/200", 0)
     }
@@ -118,7 +101,7 @@ class DarkTrafficTestServerFeatureTest extends FeatureTest with Mockito {
       liveServer.assertCounter("dark_traffic_filter/forwarded", 0)
       liveServer.assertCounter("dark_traffic_filter/skipped", 1)
 
-      server.assertHealthy() // stat to be recorded on the dark service
+      server.assertHealthy()
       // "dark" service stats
       server.assertCounter("route/delete/DELETE/status/200", 0)
     }
@@ -131,6 +114,5 @@ class DarkTrafficTestServerFeatureTest extends FeatureTest with Mockito {
 
   override def afterAll(): Unit = {
     liveServer.close()
-    server.close()
   }
 }
