c4de3fbecdaebb28aaa473aace45286c1cc36ae6
Vladimir Kostyukov <vkostyukov@twitter.com>
2017-03-30 03:16:00 +0000

    source: Migrate Finagle off HTTP codec (use HTTP stack instead)
    Disclaimer: Half of this (if not more) is done by Moses Nakamura.
    
    CSL is in the middle of [migrating Finagle HTTP from Netty 3 to Netty 4][1] and we
    want to make sure **all users** will be migrated no matter what API they use to
    configure HTTP clients/servers. Because of that we move services using HTTP codec
    (became package private as part of this RB) to HTTP stack such that they will join
    the Netty 4 migration train.
    
    [1]: https://groups.google.com/a/twitter.com/forum/#!topic/finaglers/aijSf9ZRiCw
    
    RB_ID=912427
    TBR=true
    NO_USER_HOOK=1

--- inject/inject-server/src/test/scala/com/twitter/inject/server/EmbeddedTwitterServer.scala
+++ inject/inject-server/src/test/scala/com/twitter/inject/server/EmbeddedTwitterServer.scala
@@ -4,12 +4,12 @@ import com.google.common.net.{HttpHeaders, MediaType}
 import com.google.inject.Stage
 import com.twitter.conversions.time._
 import com.twitter.finagle.builder.ClientBuilder
-import com.twitter.finagle.http._
+import com.twitter.finagle.http.{Http => _, _}
 import com.twitter.finagle.service.Backoff._
 import com.twitter.finagle.service.RetryPolicy
 import com.twitter.finagle.service.RetryPolicy._
 import com.twitter.finagle.stats.{InMemoryStatsReceiver, NullStatsReceiver, StatsReceiver}
-import com.twitter.finagle.{ChannelClosedException, Service}
+import com.twitter.finagle.{ChannelClosedException, Http, Service}
 import com.twitter.inject.PoolUtils
 import com.twitter.inject.app.{InjectionServiceWithAnnotationModule, InjectionServiceModule, StartupTimeoutException}
 import com.twitter.inject.conversions.map._
@@ -392,7 +392,7 @@ class EmbeddedTwitterServer(
     val host = new InetSocketAddress(PortUtils.loopbackAddress, port)
     val builder = ClientBuilder()
       .name(name)
-      .codec(Http(_streaming = streamResponse))
+      .stack(Http.client.withStreaming(streamResponse))
       .tcpConnectTimeout(tcpConnectTimeout)
       .connectTimeout(connectTimeout)
       .requestTimeout(requestTimeout)
