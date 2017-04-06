ff9b1361a6881532a96e2532f82c7f97d53c54cc
wolfapple <illumine03@naver.com>
2017-04-06 01:43:55 +0000

    finatra: Change the constructor parameter of HttpClientException
    Problem
    
    When using the HttpClient.executeJson method, if the status of the response
    differs from expectedStatus, an HttpClientException is returned. The status and
    contentString of the response are used when creating an HttpClientException, but
    the value of status is actually inaccessible.
    
    Solution
    
    Change the constructor parameter of HttpClientException.
    
    Signed-off-by: Yufan Gong <yufang@twitter.com>
    
    RB_ID=913964
    TBR=true

--- httpclient/src/main/scala/com/twitter/finatra/httpclient/HttpClientException.scala
+++ httpclient/src/main/scala/com/twitter/finatra/httpclient/HttpClientException.scala
@@ -4,7 +4,7 @@ import scala.util.control.NoStackTrace
 import com.twitter.finagle.http.Status
 
 class HttpClientException(
-  status: Status,
+  val status: Status,
   msg: String)
   extends Exception(msg)
   with NoStackTrace
