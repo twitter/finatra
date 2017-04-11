d0c659e3d139b7c3c1c075133c65a9828a00cbb1
Mike Solomon <msolomon@twitter.com>
2017-04-11 22:30:58 +0000

    finatra: Add default serializers for twitter-util Time and Duration
    Problem
    
    twitter-util's Time and Duration are used in some common finagle exceptions,
    such as `RequestTimeoutException`. When finatra/jackson tries to serialize
    either of these, it recurses infinitely and eventually throws a
    `StackOverflowException`.
    
    Solution
    
    Add serializers for Time and Duration to prevent the infinite recursion.
    
    Result
    
    Finatra users don't need to add custom serializers for Time and Duration.
    
    RB_ID=914150

---
+++ jackson/src/main/scala/com/twitter/finatra/json/internal/serde/TwitterUtilTimeStringSerializer.scala
@@ -0,0 +1,12 @@
+package com.twitter.finatra.json.internal.serde
+
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.ser.std.StdSerializer
+import com.twitter.util.Time
+
+private[finatra] object TwitterUtilTimeStringSerializer extends StdSerializer[Time](classOf[Time]) {
+  override def serialize(value: Time, jgen: JsonGenerator, provider: SerializerProvider) {
+    jgen.writeString(value.toString)
+  }
+}
