cd0beeaf9a8c297a6a442b3ed7dfd8ca4e70f0de
Sruthi Prabhu <sprabhu@twitter.com>
2017-10-13 23:26:40 +0000

    Adding go namespaces to be able to generate thrift for go (again)
    Summary: Adding go namespaces to be able to generate thrift for go
    
    Problem
    
    Go code generation for thrift files needs go namespaces in all thrift files and
    dependencies.
    
    Solution
    
    Added namespace go packagename to necessary thrift files. Updated pants BUILD
    targets to include golang support.
    
    Result
    
    Go code can be generated from thrift.
    
    Differential Revision: https://phabricator.twitter.biz/D95561

--- thrift/src/main/thrift/finatra-thrift/finatra_thrift_exceptions.thrift
+++ thrift/src/main/thrift/finatra-thrift/finatra_thrift_exceptions.thrift
@@ -3,6 +3,8 @@ namespace py gen.twitter.finatra.thrift.thriftpy
 #@namespace scala com.twitter.finatra.thrift.thriftscala
 #@namespace strato com.twitter.finatra.thrift
 namespace rb FinatraThrift
+# Ending a golang namespace with `thrift` breaks thrift compilation
+namespace go finatra.thriftgo
 
 exception UnknownClientIdError {
   1: string message
