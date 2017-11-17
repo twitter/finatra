5eb8e4b7dd599caed8a0d6f30b8b11ac955eaee7
Christopher Coco <ccoco@twitter.com>
2017-11-17 19:57:08 +0000

    finatra-http: Scaladoc c.t.finatra.http.filters.AccessLoggingFilter
    Summary: Problem
    
    It is not clear to some users how to determine where the
    log format is specified for the AccessLoggingFilter.
    
    Solution
    
    Add scaladocs to help bring clarity.
    
    JIRA Issues: CSL-4683
    
    Differential Revision: https://phabricator.twitter.biz/D112954

--- http/src/main/scala/com/twitter/finatra/http/filters/AccessLoggingFilter.scala
+++ http/src/main/scala/com/twitter/finatra/http/filters/AccessLoggingFilter.scala
@@ -7,6 +7,27 @@ import com.twitter.inject.Logging
 import com.twitter.util.{Future, Stopwatch}
 import javax.inject.{Inject, Singleton}
 
+/**
+ * Provides a standard "Access Log" -- a list of all requests through this Filter.
+ *
+ * NOTE: This Filter should occur as early in the Filter chain as possible.
+ * Typically, this Filter is provided by the [[com.twitter.finatra.http.modules.AccessLogModule]]
+ * which provides an implementation for the [[LogFormatter]] param as an instance of
+ * [[com.twitter.finagle.http.filter.CommonLogFormatter]].
+ *
+ * This Filter is included in the Finatra [[com.twitter.finatra.http.filters.CommonFilters]].
+ *
+ * Usage: To use, create a logger (with your preferred logging implementation) over this class
+ * which is configured to write to a specific file (usually named, `access.log`).
+ *
+ * @param logFormatter an instance of [[LogFormatter]] which specifies how log lines should be formatted.
+ * @tparam R - "Request" type param which must be a subtype of [[com.twitter.finagle.http.Request]].
+ *
+ * @see [[com.twitter.finatra.http.filters.CommonFilters]]
+ * @see [[com.twitter.finatra.http.modules.AccessLogModule]]
+ * @see [[com.twitter.finagle.filter.LogFormatter]]
+ * @see [[com.twitter.finagle.http.filter.CommonLogFormatter]]
+ */
 @Singleton
 class AccessLoggingFilter[R <: Request] @Inject()(logFormatter: LogFormatter[R, Response])
     extends SimpleFilter[R, Response]
@@ -21,7 +42,7 @@ class AccessLoggingFilter[R <: Request] @Inject()(logFormatter: LogFormatter[R, 
       service(request) onSuccess { response =>
         info(logFormatter.format(request, response, elapsed()))
       } onFailure { e =>
-        // should never get here since this filter is meant to be after the exception barrier
+        // should never get here since this filter is meant to be after the `ExceptionMappingFilter`
         info(logFormatter.formatException(request, e, elapsed()))
       }
     }
