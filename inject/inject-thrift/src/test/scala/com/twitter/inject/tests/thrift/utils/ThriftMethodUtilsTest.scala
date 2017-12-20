2761e5ab64ed8ff7c2c959b97f06c16a555ba410
Christopher Coco <ccoco@twitter.com>
2017-12-20 05:17:59 +0000

    scrooge: Support scrooge Request/Response types in generated `ThriftMethod` code
    Summary: Problem
    
    The scrooge-generated `ThriftMethod` code supports converting a `ThriftMethod` to
    it's corresponding Finagle Service construct, e.g.,
    `Service[method.Args, method.SuccessType]` given a Function1 to act over. This
    code currently still uses the deprecated naming scheme and does not support
    generation of a "ReqRep" service, i.e.,
    `Service[scrooge.Request[method.Args], scrooge.Response[method.SuccessType]]`.
    
    Solution
    
    Update the generator to emit new code that conforms to the newer naming
    scheme and add generation of the necessary "ReqRep" constructs. Deprecate
    constructs of the legacy naming scheme.
    
    Result
    
    Support for converting a `ThriftMethod` to it's corresponding Service,
    `Service[scrooge.Request[method.Args], scrooge.Response[method.SuccessType]]`
    
    Differential Revision: https://phabricator.twitter.biz/D122767

--- inject/inject-thrift/src/test/scala/com/twitter/inject/tests/thrift/utils/ThriftMethodUtilsTest.scala
+++ inject/inject-thrift/src/test/scala/com/twitter/inject/tests/thrift/utils/ThriftMethodUtilsTest.scala
@@ -1,34 +1,16 @@
 package com.twitter.inject.tests.thrift.utils
 
-import com.twitter.inject.Test
 import com.twitter.inject.thrift.utils.ThriftMethodUtils
-import com.twitter.scrooge.{ThriftStructCodec3, ThriftMethod}
+import com.twitter.inject.{Mockito, Test}
+import com.twitter.scrooge.ThriftMethod
 
-class ThriftMethodUtilsTest extends Test {
+class ThriftMethodUtilsTest extends Test with Mockito {
+  private val FakeThriftMethod = smartMock[ThriftMethod]
+  FakeThriftMethod.name returns "Foo"
+  FakeThriftMethod.serviceName returns "FooService"
 
   test("ThriftMethodUtils#return pretty string") {
-
-    val method = new ThriftMethod {
-      val name: String = "Foo"
-
-      override def annotations: Map[String, String] = ???
-
-      def toServiceIfaceService(f: FunctionType): ServiceIfaceServiceType = ???
-
-      def functionToService(f: FunctionType): ServiceType = ???
-
-      def serviceToFunction(svc: ServiceType): FunctionType = ???
-
-      val serviceName: String = "FooService"
-
-      def argsCodec: ThriftStructCodec3[Args] = ???
-
-      def responseCodec: ThriftStructCodec3[Result] = ???
-
-      val oneway: Boolean = false
-    }
-
-    val prettyString = ThriftMethodUtils.prettyStr(method)
+    val prettyString = ThriftMethodUtils.prettyStr(FakeThriftMethod)
     prettyString should be("FooService.Foo")
   }
 }
