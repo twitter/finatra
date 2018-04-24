b74630ae20d6662ea9c176139fb8b6dd9e7a044f
Dominic Mortlock <dom.mortlock@gmail.com>
2018-04-24 16:21:32 -0700

    Minor changes to naming convention and storing settings

--- httpclient/src/main/scala/com/twitter/finatra/httpclientv2/HttpClientBuilder.scala
+++ httpclient/src/main/scala/com/twitter/finatra/httpclientv2/HttpClientBuilder.scala
@@ -17,34 +17,36 @@ class HttpClientBuilder(client: Http.Client) {
   def hostname: String = ""
   def retryPolicy: Option[RetryPolicy[Try[Response]]] = None
   def defaultHeaders: Map[String, String] = Map()
+  def sslHostname: String = ""
+  def classifier: Service.ResponseClassifier = None
 
-  def setHostname(name: String): HttpClientBuilder = {
+  def withHostname(name: String): HttpClientBuilder = {
     this.hostname = hostname
     this
   }
 
-  def setRetryPolicy(retryPolicy: retryOption[RetryPolicy[Try[Response]]]): HttpClientBuilder = {
+  def withRetryPolicy(retryPolicy: retryOption[RetryPolicy[Try[Response]]]): HttpClientBuilder = {
     this.retryPolicy = retryPolicy
     this
   }
 
-  def setDefaultHeaders(defaultHeaders: Map[String, String] = Map()): HttpClientBuilder = {
+  def withDefaultHeaders(defaultHeaders: Map[String, String] = Map()): HttpClientBuilder = {
     this.defaultHeaders = defaultHeaders
     this
   }
 
-  def setTls(sslHostname: String): HttpClientBuilder = {
-    client = client.withTls(sslHostname)
+  def withTls(sslHostname: String): HttpClientBuilder = {
+    this.sslHostname = sslHostname
     this
   }
 
-  def setResponseClassifier(classifier: Service.ResponseClassifier): HttpClientBuilder = {
-    client = client.withResponseClassifier()
+  def withResponseClassifier(classifier: Service.ResponseClassifier): HttpClientBuilder = {
+    this.classifier = classifier
     this
   }
 
   def newClient(dest: String): HttpClient = {
-    val service = client.newService(dest)
+    val service = client.withTls(sslHostname).withResponseClassifier(classifier).newService(dest)
     new HttpClient(hostname = hostname, httpService = service, retryPolicy = retryPolicy, defaultHeaders = defaultHeaders)
   }
 }
