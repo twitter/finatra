0d962d0b03751e5459224dc622043d7f0f0a78bf
Mesut OZEN <mesut.ozen@trendyol.com>
2019-01-08 19:03:47 +0300

    added more test for uncovered statements

--- jackson/src/test/scala/com/twitter/finatra/json/tests/internal/caseclass/validation/validators/PatternValidatorTest.scala
+++ jackson/src/test/scala/com/twitter/finatra/json/tests/internal/caseclass/validation/validators/PatternValidatorTest.scala
@@ -32,7 +32,9 @@ class PatternValidatorTest extends ValidatorTest with GeneratorDrivenPropertyChe
   }
 
   test("fail validation when regex not matches") {
-    validate[NumberPatternExample]("meros") should equal(Invalid(errorMessage("meros", "[0-9]+"), ErrorCode.PatternNotMatched("meros", "[0-9]+")))
+    validate[NumberPatternExample]("meros") should equal(
+      Invalid(errorMessage("meros", "[0-9]+"), ErrorCode.PatternNotMatched("meros", "[0-9]+"))
+    )
   }
 
   test("fail validation when regex not matches for some items in array") {
@@ -42,12 +44,28 @@ class PatternValidatorTest extends ValidatorTest with GeneratorDrivenPropertyChe
       "invalid"
     }
     forAll(failValue) {
-      value => validate[NumberPatternArrayExample](value) should equal(Invalid(errorMessage(value.toString, "[0-9]+"), ErrorCode.PatternNotMatched("[]", "[0-9]+")))
+      value =>
+        validate[NumberPatternArrayExample](value) should equal(
+          Invalid(errorMessage(value.toString, "[0-9]+"), ErrorCode.PatternNotMatched("[]", "[0-9]+"))
+        )
     }
   }
 
   test("fail validation when given regex empty") {
-    validate[EmptyPatternExample]("1234") should equal(Invalid(PatternValidator.errorMessage(messageResolver), ErrorCode.PatternCannotBeEmpty))
+    validate[EmptyPatternExample]("1234") should equal(
+      Invalid(PatternValidator.errorMessage(messageResolver), ErrorCode.PatternCannotBeEmpty)
+    )
+  }
+
+  test("it should throw exception for invalid class type") {
+    the[IllegalArgumentException] thrownBy validate[NumberPatternArrayExample](new Object()) should have message
+      "Class [class java.lang.Object}] is not supported by class com.twitter.finatra.json.internal.caseclass.validation.validators.PatternValidator"
+  }
+
+  test("pass validation when regex matches for traversable type") {
+    forAll(Traversable("1234", "6666")) {
+      value => validate[NumberPatternArrayExample](value) should equal(Valid)
+    }
   }
 
   private def validate[C: Manifest](value: Any): ValidationResult = {
