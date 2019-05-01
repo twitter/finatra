cd2d5be357218e76204fcbbb15751119d05f826e
Dorothy Ordogh <dordogh@twitter.com>
2019-05-01 21:51:43 +0000

    finatra-http: Remove deprecated DefaultExceptionMapper
    Problem
    
    `DefaultExceptionMapper` was deprecated in 2016. It's time to remove it.
    
    Solution
    
    Remove `DefaultExceptionMapper` and replace references to it with
    `ExceptionMapper[Throwable]`
    
    Result
    
    Removed deprecated code.
    
    JIRA Issues: CSL-8031
    
    Differential Revision: https://phabricator.twitter.biz/D307520

--- http/src/main/scala/com/twitter/finatra/http/exceptions/DefaultExceptionMapper.scala
+++
@@ -1,7 +0,0 @@
-package com.twitter.finatra.http.exceptions
-
-@deprecated(
-  "Specially typing a default exception mapper is no longer necessary. Extend ExceptionMapper[Throwable] directly.",
-  "2016-09-07"
-)
-trait DefaultExceptionMapper extends ExceptionMapper[Throwable]
