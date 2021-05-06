320c8694d15801ce568df5a6a4ef9b324dd45a6b
Christopher Coco <ccoco@twitter.com>
2021-05-06 01:00:40 +0000

    finatra-jackson: Remove now unused ErrorCode class
    Problem/Solution
    
    This class is no longer used and should be removed.
    
    Differential Revision: https://phabricator.twitter.biz/D665008

--- jackson/src/main/scala/com/twitter/finatra/jackson/caseclass/exceptions/ErrorCode.scala
+++
@@ -1,8 +0,0 @@
-package com.twitter.finatra.jackson.caseclass.exceptions
-
-import com.fasterxml.jackson.core.JsonProcessingException
-import com.twitter.finatra.validation.{ErrorCode => ValidationErrorCode}
-
-object ErrorCode {
-  case class JsonProcessingError(cause: JsonProcessingException) extends ValidationErrorCode
-}
