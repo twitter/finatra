82531fa369c3ecb7a00f2219130503c0aa56a646
Ian Bennett <ibennett@twitter.com>
2021-06-15 02:02:56 +0000

    finatra/jackson: fix Annotations cache leak
    Problem
    
    The `Annotations` cache is leaking resources.
    `BeanProperty` does not implement `hashCode` and
    `equals` method, which means that we are generating
    a new cache entry on every call and retaining the entry
    in the cache indefinitely, which will eventually result in
    an `OutOfMemory` (or GC spiral). We aren't gaining
    any performance improvement from the cache due to
    this.
    
    Solution
    
    Let's remove the cache and resolve the leak.
    
    JIRA Issues: CSL-11047
    
    Differential Revision: https://phabricator.twitter.biz/D688123

--- jackson/src/main/scala/com/twitter/finatra/jackson/caseclass/Annotations.scala
+++ jackson/src/main/scala/com/twitter/finatra/jackson/caseclass/Annotations.scala
@@ -1,18 +1,11 @@
 package com.twitter.finatra.jackson.caseclass
 
 import com.fasterxml.jackson.databind.BeanProperty
-import com.twitter.util.Memoize
 import java.lang.annotation.Annotation
 
 private[finatra] object Annotations {
-  private case class HasAnnotation(
-    beanProperty: BeanProperty,
-    annotations: Seq[Class[_ <: Annotation]])
 
   def hasAnnotation(beanProperty: BeanProperty, annotations: Seq[Class[_ <: Annotation]]): Boolean =
-    hasAnnotation(HasAnnotation(beanProperty, annotations))
+    annotations.exists(beanProperty.getContextAnnotation(_) != null)
 
-  private val hasAnnotation: HasAnnotation => Boolean = Memoize { hasAnnotation =>
-    hasAnnotation.annotations.exists(hasAnnotation.beanProperty.getContextAnnotation(_) != null)
-  }
 }
